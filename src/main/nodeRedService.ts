e9b14422eb8583412b81dbfd525acbebec86a753
Jennifer Fix <codingjennifer@gmail.com>
2025-01-04 14:38:17 -0700

    node-red works

--- src/main/nodeRedService.ts
+++ src/main/nodeRedService.ts
@@ -1,28 +1,41 @@
 import * as RED from 'node-red'
 import path from 'path'
 import { app } from 'electron'
+import * as http from 'http'
+import express from 'express'
+import { type LocalSettings } from '@node-red/runtime'
 
 export async function startNodeRed() {
   const userDir = path.join(app.getPath('userData'), 'node-red')
-  const settings = {
+  const settings: LocalSettings = {
     httpAdminRoot: '/red',
     httpNodeRoot: '/api',
     userDir,
     flowFile: 'flows.json',
-    editorTheme: {
-      header: {
-        title: 'JAHMI NODE RED'
-      }
-    },
+    // editorTheme: {
+    //   header: {
+    //     title: 'JAHMI NODE RED'
+    //   }
+    // },
     functionGlobalContext: {},
-    port: 1880
+    uiHost: 'localhost',
+    uiPort: 1880
   }
 
   return new Promise((resolve, reject) => {
     try {
-      RED.init({}, settings)
-      const server = RED.start()
-      resolve(server)
+      const expressApp = express()
+      const httpserver = http.createServer(expressApp)
+      RED.init(httpserver, settings)
+      expressApp.use(settings.httpNodeRoot as string, RED.httpNode)
+      expressApp.use(settings.httpAdminRoot as string, RED.httpAdmin)
+      RED.start().then(() => {
+        // start
+        httpserver.listen(settings.uiPort, () => {
+          console.log(`Node-RED running on port ${settings.uiPort}`)
+        })
+      })
+      resolve(httpserver)
     } catch (error) {
       reject(error)
     }
