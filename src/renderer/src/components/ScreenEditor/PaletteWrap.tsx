15fd7aa651d766f061ade5d198e997ec9810268a
JenniferFix <codingjennifer@gmail.com>
2025-01-09 11:13:21 -0700

    work

--- src/renderer/src/components/ScreenEditor/PaletteWrap.tsx
+++ src/renderer/src/components/ScreenEditor/PaletteWrap.tsx
@@ -2,20 +2,27 @@ import React from 'react'
 import Draggable, { DraggableEventHandler } from 'react-draggable'
 
 const PaletteOutline = ({ title, children }: { title: string; children: React.ReactNode }) => {
+  const ref = React.useRef(null)
   const handleDragStart: DraggableEventHandler = (e, data) => {
     //
-    console.log('e', e)
-    console.log('data', data)
+    //console.log('e', e)
+    //console.log('data', data)
+    if (data && data.node) {
+      data.node.style.pointerEvents = "none"
+    }
   }
   const handleDragEnd: DraggableEventHandler = (e, data) => {
     //
-    console.log('e', e)
-    console.log('data', data)
+    //console.log('e', e)
+    //console.log('data', data)
+    if (data && data.node) {
+      data.node.style.pointerEvents = "auto"
+    }
   }
 
   return (
-    <Draggable handle=".draggable-handle" onStart={handleDragStart} onStop={handleDragEnd()}>
-      <div className="absolute inset-0 flex flex-col border border-muted-50">
+    <Draggable handle=".draggable-handle" onStart={handleDragStart} onStop={handleDragEnd()} nodeRef={ref}>
+      <div ref={ref} className="absolute inset-0 flex flex-col border border-muted-50">
         <div className="draggable-handle bg-muted p-1 pl-3 shrink-0 text-sm font-semibold">
           {title}
         </div>
