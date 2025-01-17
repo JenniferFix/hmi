851e867e574c62665aedc6aeb5871cde9487c6b5
Jennifer Fix <codingjennifer@gmail.com>
2025-01-16 21:53:46 -0700

    workspace stuff, updates, start ts setup

---
+++ packages/node-red-contrib-fixtech-hmi-websocket/widget.js
@@ -0,0 +1,11 @@
+export default function (RED) {
+  function WidgetNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    node.on("input", function (msg) {
+      msg.payload = msg.payload.toLowerCase();
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("widget", WidgetNode);
+}
