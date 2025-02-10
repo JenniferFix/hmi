9ed95c8d1388379870df18dd4356bb7cd85fde37
Jennifer Fix <codingjennifer@gmail.com>
2025-02-10 00:00:15 -0700

    update config page

--- packages/hmi-electron/src/renderer/src/components/config/ConfigPage.tsx
+++ packages/hmi-electron/src/renderer/src/components/config/ConfigPage.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { ThemeToggle } from '@renderer/components/common/ThemeToggle'
+import { Table, TableRow, TableCell, TableBody } from '@renderer/components/ui/table'
 
 const ConfigPage = () => {
   return (
     <div>
       <h2 className="text-xl">Config</h2>
-      <div className="flex align-middle max-w-lg">
-        <div>Toggle Theme</div>
-        <div>
-          <ThemeToggle />
-        </div>
-      </div>
+      <Table className="max-w-md">
+        <TableBody>
+          <TableRow>
+            <TableCell>Toggle Theme</TableCell>
+            <TableCell>
+              <ThemeToggle />
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <div className="flex items-center max-w-lg"></div>
     </div>
   )
 }
